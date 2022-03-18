86fe97fc8f98a428c75191eabf0b54c6b9450878
Halid_Macalangcom <vmhunterjob@gmail.com>
2022-03-19 01:34:39 +0800

    20

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import "./App.css";
 import { useState } from 'react'
 function App() {
   const [name, setName] = useState('mario')
+  const [events, setEvents] = useState([
+    {title: "mario's birthday bash", id: 1},
+    {title: "bowser's live stream", id: 2},
+    {title: "race on moo moo farm", id: 3}
+  ])
 
   const handleClick = () => {
     setName('batman')
@@ -12,6 +17,11 @@ function App() {
     <div className="App">
       <h1>My name is {name}</h1>
       <button onClick={handleClick}>Change name</button>
+      {events.map((event) => (
+        <div key={event.id}>
+          <h2>{event.title}</h2>
+        </div>
+      ))}
     </div>
   );
 }
