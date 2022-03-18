9e056c8ac0dbbc40c3384c34eee169cbf1324e53
Halid_Macalangcom <vmhunterjob@gmail.com>
2022-03-19 00:33:58 +0800

    17

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  let name = 'mario'
+  
+  const handleClick = () => {
+    name = 'luigi'
+    console.log(name);
+  }
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My name is {name}</h1>
+      <button onClick={handleClick}>Change name</button>
     </div>
   );
 }
