4805efe77ee3ed128d2d6b2372ae8f8195518caa
Halid_Macalangcom <vmhunterjob@gmail.com>
2022-03-22 06:39:41 +0800

    25

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
 import { useState } from "react";
+import Title from './components/Title'
+
 function App() {
   const [showEvents, setShowEvents] = useState(true);
   const [events, setEvents] = useState([
@@ -18,6 +20,7 @@ function App() {
   };
   return (
     <div className="App">
+      <Title />
       {showEvents && (
         <div>
         <button onClick={() => setShowEvents(false)}>Hide Events</button>
