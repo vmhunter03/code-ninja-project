8be0aeb707a4f8f6a25e8078737e5ca2af547eb1
Halid_Macalangcom <vmhunterjob@gmail.com>
2022-03-23 04:58:08 +0800

    35

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,6 +1,7 @@
+import './Title.css'
 export default function Title( {title, subtitle }) {
     return (
-        <div>
+        <div className='title-block'>
             <h1 className="title">{title}</h1>
             <br />
             <h2 className="subtitle">{subtitle}</h2>
